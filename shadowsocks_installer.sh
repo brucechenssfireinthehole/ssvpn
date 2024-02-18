c4bbe8d2f9d50618cc6639dc2d04193934c2e320
Dosu <35868127+brucechenssfireinthehole@users.noreply.github.com>
2024-02-18 15:50:57 +0800

    Update shadowsocks_installer.sh

--- shadowsocks_installer.sh
+++ shadowsocks_installer.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 #This is a shell script for deploy shadowsocks server.
 
+sudo echo "y" | yum install expect 
+
 #1-create reboot scirpt
 touch shadowsocks_installer_inner.sh
 chmod 777 shadowsocks_installer_inner.sh
 echo "#!/bin/bash
+echo \"hello world\" > helloworld.txt
 #install shadowsocks server
-echo \"test restart\"
 wget --no-check-certificate -O shadowsocks.sh https://raw.githubusercontent.com/brucechenssfireinthehole/ssvpn/main/shadowsocks.sh
 chmod +x shadowsocks.sh
 
 expect -c \"
-spawn ./shadowsocks.sh 2>&1 | tee shadowsocks.log
+spawn ./shadowsocks.sh
 expect \"Please enter password for shadowsocks-python\" {send \"123456\n\"}
 expect \"Please enter a port for shadowsocks-python\" {send \"6000\n\"}
 expect \"Please select stream cipher for\" {send \"1\n\"}
@@ -30,8 +32,10 @@ Name=shadowsocks_installer_inner" > shadowsocks_installer_inner.desktop
 sudo -cp -f shadowsocks_installer_inner.desktop /etc/xdg/autostart/
 
 #2-install RuiSu speeder
+wget -N  --no-check-certificate https://raw.githubusercontent.com/91yun/serverspeeder/master/serverspeeder-all.sh
+chmod +x serverspeeder-all.sh
 expect -c "
-spawn wget -N  --no-check-certificate https://raw.githubusercontent.com/91yun/serverspeeder/master/serverspeeder-all.sh && bash serverspeeder-all.sh
+spawn bash serverspeeder-all.sh
 expect \"Do you want to restart system?\" {send \"y\n\"}
 expect eof
 "
