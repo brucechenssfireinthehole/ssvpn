cad34daf9b50e5cb46a57f589b59b3a735606d9e
Dosu <35868127+brucechenssfireinthehole@users.noreply.github.com>
2024-03-09 13:35:08 +0800

    Update ssr_installer.sh

--- ssr_installer.sh
+++ ssr_installer.sh
@@ -48,14 +48,15 @@ expect \"请输入数字\" {send \"1\n\"}
 expect \"端口\" {send \"2333\n\"}
 expect \"密码\" {send \"123456\n\"}
 expect \"加密方式\" {send \"1\n\"}
-expect \"协议插件\" {send \"5\n\"}
-expect \"协议兼容原版\" {send \"y\n\"}
+expect \"协议插件\" {send \"1\n\"}
+expect \"混淆插件\" {send \"5\n\"}
+expect \"混淆插件兼容原版\" {send \"y\n\"}
 expect \"设备数限制\" {send \"\n\"}
 expect \"单线程限速\" {send \"\n\"}
 expect \"端口总限速\" {send \"\n\"}
 interact
 "
-sleep 20s
+# sleep 20s
 wget -N --no-check-certificate "https://raw.githubusercontent.com/brucechenssfireinthehole/ssvpn/main/tcp.sh"
 chmod +x tcp.sh
 sudo echo "4" | bash ./tcp.sh
