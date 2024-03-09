9fa7cc5138460eb983b2c04e99f18d3afa922e75
Dosu <35868127+brucechenssfireinthehole@users.noreply.github.com>
2024-03-09 13:29:22 +0800

    Update ssr_installer.sh

--- ssr_installer.sh
+++ ssr_installer.sh
@@ -48,19 +48,14 @@ expect \"请输入数字 [1-15]\" {send \"1\n\"}
 expect \"端口\" {send \"2333\n\"}
 expect \"密码\" {send \"123456\n\"}
 expect \"加密方式\" {send \"1\n\"}
-expect \"协议插件\" {send \"4\n\"}
+expect \"协议插件\" {send \"5\n\"}
 expect \"协议兼容原版\" {send \"y\n\"}
 expect \"设备数限制\" {send \"\n\"}
 expect \"单线程限速\" {send \"\n\"}
 expect \"端口总限速\" {send \"\n\"}
 interact
 "
+sleep 20s
 wget -N --no-check-certificate "https://raw.githubusercontent.com/brucechenssfireinthehole/ssvpn/main/tcp.sh"
 chmod +x tcp.sh
-# expect -c "
-# spawn ./tcp.sh
-# expect \"请输入数字 [0-11]\" {send \"4\n\"}
-
-# "
 sudo echo "4" | bash ./tcp.sh
-# expect \"Do you want to restart system\" {send \"y\n\"}
